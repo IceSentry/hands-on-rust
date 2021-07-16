7dcd8ca24e15b9312912020dffe386fb258dfc6a
Charles Giguère <c.giguere42@gmail.com>
2021-07-16 13:03:17 -0400

    fix state management

--- src/flappy_plugin.rs
+++ src/flappy_plugin.rs
@@ -15,9 +15,23 @@ impl Plugin for FlappyPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app.add_state(GameState::Menu)
             .add_system_set(SystemSet::on_update(GameState::Menu).with_system(menu.system()))
+            .add_system_set(SystemSet::on_exit(GameState::Menu).with_system(clear_input.system()))
             .add_system_set(SystemSet::on_update(GameState::Playing).with_system(play.system()))
-            .add_system_set(SystemSet::on_update(GameState::End).with_system(end.system()));
+            .add_system_set(
+                SystemSet::on_exit(GameState::Playing).with_system(clear_input.system()),
+            )
+            .add_system_set(SystemSet::on_update(GameState::End).with_system(end.system()))
+            .add_system_set(SystemSet::on_exit(GameState::End).with_system(clear_input.system()));
     }
+}
+
+fn clear_input(mut keyboard_input: ResMut<Input<KeyCode>>) {
+    keyboard_input.update();
+}
+
+fn restart(state: &mut State<GameState>) {
+    info!("Changing state to Playing");
+    state.set(GameState::Playing).expect("failed to set state");
 }
 
 fn menu(
@@ -31,16 +45,11 @@ fn menu(
     ctx.print_centered(8, "(P) Play Game");
     ctx.print_centered(9, "(Q) Quit Game");
 
-    if keyboard_input.pressed(KeyCode::P) {
+    if keyboard_input.just_pressed(KeyCode::P) {
         restart(&mut state);
-    } else if keyboard_input.pressed(KeyCode::Q) {
+    } else if keyboard_input.just_pressed(KeyCode::Q) {
         app_exit_events.send(AppExit);
     }
-}
-
-fn restart(state: &mut State<GameState>) {
-    info!("Changing state to Playing");
-    state.set(GameState::Playing).expect("failed to set state");
 }
 
 fn play(mut state: ResMut<State<GameState>>) {
@@ -55,13 +64,14 @@ fn end(
     mut app_exit_events: EventWriter<AppExit>,
 ) {
     ctx.cls();
+    ctx.print(0, 0, "end");
     ctx.print_centered(5, "You are dead");
     ctx.print_centered(8, "(P) Play Game");
     ctx.print_centered(9, "(Q) Quit Game");
 
-    if keyboard_input.pressed(KeyCode::P) {
+    if keyboard_input.just_pressed(KeyCode::P) {
         restart(&mut state);
-    } else if keyboard_input.pressed(KeyCode::Q) {
+    } else if keyboard_input.just_pressed(KeyCode::Q) {
         app_exit_events.send(AppExit);
     }
 }
